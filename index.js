39f8867b6eee80e10b5a8842209ab30c7a147103
Loid Padre <145937391+loidpadre@users.noreply.github.com>
2024-06-24 20:57:29 +0000

    Ajustes para o deploy

--- index.js
+++ index.js
@@ -2,73 +2,73 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Home = require("./model/homesModel");
-const cors = require("cors")
+const cors = require("cors");
 
-require('dotenv').config();
+const port = 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
-//pegar todos os dados
-app.get("/", async (req, res) =>{
-    try {
-        const homes = await Home.find({})
-        res.status(200).json(homes)
-        
-    } catch (error) {
-        console.log("Erro ao carregar casas")
-        res.status(500).json({message: "Eu a carregar dados"})
-    }
+// pegar todos os dados
+app.get("/", async (req, res) => {
+  try {
+    const homes = await Home.find({});
+    res.status(200).json(homes);
+  } catch (error) {
+    console.log("Erro ao carregar casas", error);
+    res.status(500).json({ message: "Erro ao carregar dados" });
+  }
 });
+
 // pegar um unico dado
-app.get("/detail/:id", async (req, res) =>{
-    const {id} = req.params
-    try {
-        const home = await Home.findById(id)
-        res.status(200).json(home)
-        
-    } catch (error) {
-        console.log("Erro ao carregar casa uniica")
-        res.status(500).json({message: "Eu a carregar dados da casa unica"})
-    }
+app.get("/detail/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const home = await Home.findById(id);
+    res.status(200).json(home);
+  } catch (error) {
+    console.log("Erro ao carregar casa unica", error);
+    res.status(500).json({ message: "Erro ao carregar dados da casa unica" });
+  }
 });
 
 // deletar um dado
 app.delete("/dash/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        // Usar { _id: id } para especificar o documento a ser deletado
-        const home = await Home.deleteOne({ _id: id });
-        if (home.deletedCount === 0) {
-            // Se nenhum documento foi deletado, pode ser porque o id não foi encontrado
-            return res.status(404).json({ message: "Casa não encontrada" });
-        }
-        // Sucesso, documento deletado
-        res.status(200).json({ message: "Casa deletada com sucesso" });
-    } catch (error) {
-        console.log("Erro ao deletar casa", error);
-        res.status(500).json({ message: "Erro ao deletar casa" });
+  const { id } = req.params;
+  try {
+    const home = await Home.deleteOne({ _id: id });
+    if (home.deletedCount === 0) {
+      return res.status(404).json({ message: "Casa não encontrada" });
     }
+    res.status(200).json({ message: "Casa deletada com sucesso" });
+  } catch (error) {
+    console.log("Erro ao deletar casa", error);
+    res.status(500).json({ message: "Erro ao deletar casa" });
+  }
 });
 
 // cadastrar uma nova casa
-app.post("/dash/add", async (req, res) =>{
-    try {
-        const home = await Home.create(req.body);
-        res.status(200).json(home); 
-    } catch (error) {
-        console.log("Erro a cadastrar casa" + error);
-        res.status(500).json({message: "Erro a cadastrar casa"}); 
-    }
+app.post("/dash/add", async (req, res) => {
+  try {
+    const home = await Home.create(req.body);
+    res.status(200).json(home);
+  } catch (error) {
+    console.log("Erro a cadastrar casa", error);
+    res.status(500).json({ message: "Erro a cadastrar casa" });
+  }
 });
 
-mongoose.connect(process.env.MONGODB_URI).then(() =>{
-    app.listen(process.env.PORT, ()=>{
-        console.log(`servidor rodando e DB conectada na porta ${process.env.PORT}`);
+mongoose
+  .connect(
+    "mongodb+srv://loidpadre:N04lFkInEbVJgiXr@newhabitue.wa2czlp.mongodb.net/?retryWrites=true&w=majority&appName=newhabitue",
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Servidor rodando e DB conectada na porta ${port}`);
     });
-}).catch((err) =>{
-    console.log("erro ao se conectar a DB");
-});
+  })
+  .catch((err) => {
+    console.log("Erro ao se conectar ao DB", err);
+  });
 
 module.exports = app;
